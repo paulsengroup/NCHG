a542236030bcd7e694f72831dc91968a571a01d6
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-04-08 17:26:46 +0200

    Add workflow to build NCHG's Docker image

---
+++ utils/devel/test_docker_image.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2024 Roberto Rossini <roberros@uio.no>
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# This library is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see
+# <https://www.gnu.org/licenses/>.
+
+set -eu
+set -o pipefail
+
+if [ $# -ne 1 ]; then
+  2>&1 echo "Usage: $0 nchg:latest"
+  exit 1
+fi
+
+IMG="$1"
+
+tmpdir="$(mktemp -d)"
+trap "rm -rf '$tmpdir'" EXIT
+
+cat > "$tmpdir/runme.sh" <<- 'EOM'
+
+set -eu
+
+whereis -b NCHG
+NCHG --help
+NCHG --version
+
+EOM
+
+chmod 755 "$tmpdir/runme.sh"
+
+sudo docker run --rm --entrypoint=/bin/bash \
+  -v "$tmpdir/runme.sh:/tmp/runme.sh:ro" \
+  -v "$PWD/test/scripts:/tmp/nghc/test/scripts:ro" \
+  "$IMG" \
+  /tmp/runme.sh

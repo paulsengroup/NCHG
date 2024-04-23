9acb43570978bed6a4b48078dec64175d4634002
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-04-23 18:19:01 +0200

    Workaround rare Bad file descriptor exceptions
    This bug occurred only on one on my machines when running NCHG using
    apptainer.
    The exception was raised upon process startup.
    Using boost::process v1 seems to solve the issue.

--- src/nchg/compute/compute.cpp
+++ src/nchg/compute/compute.cpp
@@ -37,13 +37,11 @@
 #endif
 
 // clang-format off
-// As of HighFive 2.9.0, these header must be included after HighFive/hictk,
+// As of HighFive 2.9.0, these headers must be included after HighFive/hictk,
 // otherwise this source file fails to compile with MSVC
-#include <boost/asio/readable_pipe.hpp>
-#include <boost/asio/read_until.hpp>
-#include <boost/asio/streambuf.hpp>
-#include <boost/process/v2/process.hpp>
-#include <boost/process/v2/stdio.hpp>
+#include <boost/process/child.hpp>
+#include <boost/process/pipe.hpp>
+#include <boost/process/io.hpp>
 // clang-format on
 
 #include "nchg/common.hpp"
@@ -66,6 +64,7 @@ static void print_header() {
                  "expected_count\t"
                  "odds_ratio\t"
                  "omega\n"));
+  fflush(stdout);
 }
 
 [[nodiscard]] static std::string_view truncate_bed3_record(std::string_view record,
@@ -296,8 +295,7 @@ static void io_worker(moodycamel::BlockingConcurrentQueue<std::string> &msg_queu
 
 [[nodiscard]] static auto spawn_compute_process(const ComputePvalConfig &c,
                                                 const std::string &chrom1,
-                                                const std::string &chrom2,
-                                                boost::asio::io_context &ctx) {
+                                                const std::string &chrom2) {
   SPDLOG_INFO(FMT_STRING("begin processing {}:{}"), chrom1, chrom2);
 
   std::vector<std::string> args{"compute",
@@ -329,9 +327,10 @@ static void io_worker(moodycamel::BlockingConcurrentQueue<std::string> &msg_queu
     args.emplace_back(c.path_to_expected_values.string());
   }
 
-  boost::asio::readable_pipe pipe(ctx);
-  boost::process::v2::process proc(ctx, c.exec.string(), args,
-                                   boost::process::v2::process_stdio{{}, pipe, {}});
+  boost::process::ipstream pipe;
+  boost::process::child proc(
+      c.exec.string(), args,
+      boost::process::std_in<boost::process::null, boost::process::std_out> pipe);
   SPDLOG_DEBUG(FMT_STRING("spawned worker process {}..."), proc.id());
   if (!proc.running()) {
     throw std::runtime_error(fmt::format(FMT_STRING("failed to spawn worker process: {} {}"),
@@ -342,26 +341,15 @@ static void io_worker(moodycamel::BlockingConcurrentQueue<std::string> &msg_queu
 }
 
 static void consume_compute_process_output(
-    boost::process::v2::process &proc, boost::asio::readable_pipe &pipe,
+    boost::process::child &proc, boost::process::ipstream &pipe,
     [[maybe_unused]] std::string_view chrom1, [[maybe_unused]] std::string_view chrom2,
     moodycamel::BlockingConcurrentQueue<std::string> &msg_queue, std::atomic<bool> &early_return,
     std::atomic<std::size_t> &msg_submitted) {
-  boost::system::error_code ec{};
-  boost::asio::streambuf strbuff;
-  std::istream is(&strbuff);
   std::string line;
 
   std::size_t records_processed{};
   while (true) {
-    boost::asio::read_until(pipe, strbuff, '\n', ec);
-    if (ec) {
-      early_return = true;
-      if (proc.running()) {
-        proc.terminate();
-      }
-      throw std::runtime_error(ec.message());
-    }
-    std::getline(is, line);
+    std::getline(pipe, line);
 
     if (line == "__EOF__" || line == "__EOF__\r") {
       break;
@@ -387,7 +375,7 @@ static void consume_compute_process_output(
 
 template <typename PidT>
 [[nodiscard]] static std::size_t register_process(
-    [[maybe_unused]] boost::process::v2::process &proc, [[maybe_unused]] std::atomic<PidT *> &pids,
+    [[maybe_unused]] boost::process::child &proc, [[maybe_unused]] std::atomic<PidT *> &pids,
     [[maybe_unused]] const std::atomic<std::size_t> &num_pids,
     [[maybe_unused]] std::mutex &pids_mtx) {
 #ifdef _WIN32
@@ -445,8 +433,7 @@ static std::size_t process_queries(
         const auto chrom2 = std::string{chrom_pairs[i].second.name()};
 
         try {
-          boost::asio::io_context ctx{};
-          auto [pipe, proc] = spawn_compute_process(c, chrom1, chrom2, ctx);
+          auto [pipe, proc] = spawn_compute_process(c, chrom1, chrom2);
           const auto pid_offset = register_process(proc, pids, num_pids, pids_mtx);
 
           consume_compute_process_output(proc, pipe, chrom1, chrom2, msg_queue, early_return,
@@ -462,9 +449,12 @@ static std::size_t process_queries(
                 fmt::format(FMT_STRING("child process terminated with code {}"), proc.exit_code()));
           }
         } catch (const std::exception &e) {
+          early_return = true;
           throw std::runtime_error(
               fmt::format(FMT_STRING("error in the worker thread processing {}:{}: {}"), chrom1,
                           chrom2, e.what()));
+        } catch (...) {
+          early_return = true;
         }
       },
       chrom_pairs.size());

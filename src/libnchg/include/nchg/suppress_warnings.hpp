99bd52ce48d7822c656cb70317ae154b849f75f2
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-10-26 17:38:29 +0200

    Address compiler warnings

---
+++ src/libnchg/include/nchg/suppress_warnings.hpp
@@ -0,0 +1,66 @@
+// Copyright (C) 2024 Roberto Rossini <roberros@uio.no>
+//
+// SPDX-License-Identifier: GPL-3.0
+//
+// This library is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Public License as published
+// by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Public License along
+// with this library.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+// Source: https://www.fluentcpp.com/2019/08/30/how-to-disable-a-warning-in-cpp/
+// GCC to MSVC codes: https://github.com/srz-zumix/awesome-cpp-warning
+
+// clang-format off
+
+// NOLINTBEGIN(cppcoreguidelines-macro-usage)
+
+// Defines for MSVC
+#ifdef _MSC_VER
+    #define NCHG_DISABLE_WARNING_PUSH                      __pragma(warning(push))
+    #define NCHG_DISABLE_WARNING_POP                       __pragma(warning(pop))
+    #define NCHG_DISABLE_WARNING(warningNumber)            __pragma(warning(disable : warningNumber))
+
+    #define NCHG_DISABLE_WARNING_DEPRECATED_DECLARATIONS   NCHG_DISABLE_WARNING(4996)
+#endif
+
+// Defines for GCC and Clang
+#if defined(__GNUC__) || defined(__clang__)
+    #define NCHG_DO_PRAGMA(X)                              _Pragma(#X)
+    #define NCHG_DISABLE_WARNING_PUSH                      NCHG_DO_PRAGMA(GCC diagnostic push)
+    #define NCHG_DISABLE_WARNING_POP                       NCHG_DO_PRAGMA(GCC diagnostic pop)
+    #define NCHG_DISABLE_WARNING(warningName)              NCHG_DO_PRAGMA(GCC diagnostic ignored warningName)
+
+    #define NCHG_DISABLE_WARNING_DEPRECATED_DECLARATIONS   NCHG_DISABLE_WARNING("-Wdeprecated-declarations")
+#endif
+
+// Defines for GCC only
+#if defined(__GNUC__) && !defined(__clang__)
+#endif
+
+// Defines for Clang only
+#ifdef __clang__
+#endif
+
+// Defines for unknown/unsupported compilers
+#if !defined(_MSC_VER) && !defined(__GNUC__) && !defined(__clang__)
+    #define NCHG_DISABLE_WARNING
+    #define NCHG_DISABLE_WARNING_PUSH
+    #define NCHG_DISABLE_WARNING_POP
+
+    #define NCHG_DISABLE_WARNING_DEPRECATED_DECLARATIONS
+#endif
+
+// NOLINTEND(cppcoreguidelines-macro-usage)
+
+// clang-format on
